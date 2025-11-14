4a4250b4c2dbaba845840736ef342f9e82304ac2
Kaylen Ton <kaylent2k@gmail.com>
2025-11-14 13:18:16 -0500

    pasting together df + visualizing variables for assessing distribution

--- EXPERIMENTING --- WILL DELETE/Toymaker dataset .R
+++ EXPERIMENTING --- WILL DELETE/Toymaker dataset .R
@@ -1,19 +1,48 @@
-# Toymaker dataset
-set.seed(67)
+# toymaker dataset
+set.seed(6767)
 
 # elf identifier variable. 1225 observations
-elf_id <- 0001:1225
+total_elves <- 1225
+elf_id <- 0001:total_elves
 
 # sex variable
-sex <- c("Male", "Female")
-sex <- sample(sex,1225,T)
+sex <- as.factor(sample(c("Male", "Female"), total_elves, T))
 table(sex) # table() shows the count per category
 
+# age
+age <- pmax(round(rnorm(total_elves, mean = 3140, sd = 1000)), 100)
+hist(age)
+
+# job_type
+job_type <- as.factor(sample(c("Full-time", "Part-time"), total_elves, T, prob = c(.75, .25)))
+table(job_type)
+
+# schedule
 days <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday") # Sundays are off
 # Elves are either part-time or full-time
 
-age <- sample(100:5000, 1225, T)
+# creating dataframe + new variable: weekly_hours
+toymaker <- data.frame(elf_id, sex, age, job_type) %>%
+  mutate(weekly_hours = ifelse(job_type == "Full-time",
+                               pmin(pmax(round(rnorm(n(), mean = 40, sd = 10)), 40), 60),
+                               pmin(pmax(round(rnorm(n(), mean = 30, sd = 3)), 20), 34)
+                              )
+         )
+
+# Test Visualizations (for assessing distribution purposes)
+toymaker %>%
+  select(job_type, weekly_hours) %>%
+  table()
+
+hist(toymaker$weekly_hours[toymaker$job_type == "Full-time"])
+hist(toymaker$weekly_hours[toymaker$job_type == "Part-time"])
+hist(toymaker$weekly_hours)
+
+toymaker %>%
+  filter(age <= 1000) %>%
+  group_by(job_type) %>%
+  arrange(age) %>%
+  count(job_type)
 
 
-# Creating Dataframe
-toymaker <- data.frame(elf_id, sex, age)
+
