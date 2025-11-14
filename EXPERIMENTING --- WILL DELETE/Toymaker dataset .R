87c09fd3501a18f08de2f7220d11f536a21b48bb
Kaylen Ton <kaylent2k@gmail.com>
2025-11-14 00:51:45 -0500

    table()

--- EXPERIMENTING --- WILL DELETE/Toymaker dataset .R
+++ EXPERIMENTING --- WILL DELETE/Toymaker dataset .R
@@ -1,14 +1,15 @@
 # Toymaker dataset
 set.seed(67)
 
+# elf identifier variable. 1225 observations
 elf_id <- 0001:1225
 
-sex <- c("Male", "Female", "Non-Binary")
+# sex variable
+sex <- c("Male", "Female")
 sex <- sample(sex,1225,T)
+table(sex) # table() shows the count per category
 
 days <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday") # Sundays are off
 # Elves are either part-time or full-time
 
 age <- rnorm(100:4500, 1225, T)
-
-
