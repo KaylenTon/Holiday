5b1dd21c8e9e3b2820ba668684d31649ce755195
Kaylen Ton <kaylent2k@gmail.com>
2025-11-17 12:32:00 -0500

    fixed "next year" holidays

--- R/holiday_countdown.R
+++ R/holiday_countdown.R
@@ -3,16 +3,18 @@ holiday_countdown <- function(holiday) {
   today <- Sys.Date()
   current_year <- format(today, "%Y")
 
-  holiday_date <- holiday_list[[holiday]]
-  holiday_date <- as.Date(paste0(current_year, "-", holiday_date))
+  month_day <- holiday_list[[holiday]]
+  holiday_date <- as.Date(paste0(current_year, "-", month_day))
 
   if (holiday_date < today) {
     next_year <- as.numeric(current_year) + 1
-    holiday_date <- as.Date(paste0(current_year, "-", holiday_date))
+    holiday_date <- as.Date(paste0(next_year, "-", month_day))
   }
 
   daysLeft <- as.numeric(holiday_date - today)
 
-  print(paste("There are", daysLeft, "days until", holiday, "!"))
+  print(paste0("There are ", daysLeft, " days until ", holiday, "!"))
 
 }
+
+holiday_countdown("NewYearsDay")
