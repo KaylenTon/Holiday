70843c703928bfd548747526dbc5b78deaef31c1
Kaylen Ton <kaylent2k@gmail.com>
2025-11-23 19:36:28 -0500

    rename skewed age variable to just age

--- EXPERIMENTING --- WILL DELETE/Toymaker dataset .R
+++ EXPERIMENTING --- WILL DELETE/Toymaker dataset .R
@@ -11,21 +11,17 @@ sex <- as.factor(sample(c("Male", "Female"), total_elves, T))
 table(sex) # table() shows the count per category
 
 # age
-age <- pmax(round(rnorm(total_elves, mean = 3140, sd = 1000)), 100)
-hist(age)
-
-# skewed age
 library(sn)
-skewedAge <- pmax((round(rsn(total_elves, xi = c(100:total_elves), omega = 1200, alpha = 10, dp = NULL))), 100)
-summary(skewedAge)
-hist(skewedAge)
+age <- pmax((round(rsn(total_elves, xi = c(100:total_elves), omega = 1200, alpha = 10, dp = NULL))), 100)
+summary(age)
+hist(age)
 
 # job_type
 job_type <- as.factor(sample(c("Full-time", "Part-time"), total_elves, T, prob = c(.75, .25)))
 table(job_type)
 
 # creating dataframe + new variable: weekly_hours
-toymaker <- data.frame(elf_id, sex, skewedAge, job_type) %>%
+toymaker <- data.frame(elf_id, sex, age, job_type) %>%
   mutate(weekly_hours = ifelse(job_type == "Full-time",
                                pmin(pmax(round(rnorm(n(), mean = 40, sd = 10)), 40), 60),
                                pmin(pmax(round(rnorm(n(), mean = 30, sd = 3)), 20), 34)
@@ -38,12 +34,12 @@ toymaker <- toymaker %>%
   rowwise() %>%
   mutate(
     work_experience = case_when(
-      skewedAge <= 500 ~ round(runif(1, 75, skewedAge - 25)),
-      skewedAge <= 1000 ~ round(runif(1, 476, skewedAge - 25)),
-      skewedAge <= 2000 ~ round(runif(1, 976, skewedAge - 25)),
-      skewedAge <= 3000 ~ round(runif(1, 1976, skewedAge - 25)),
-      skewedAge <= 4000 ~ round(runif(1, 2976, skewedAge - 25)),
-      TRUE        ~ round(runif(1, 3976, skewedAge - 25))
+      age <= 500 ~ round(runif(1, 75, age - 25)),
+      age <= 1000 ~ round(runif(1, 476, age - 25)),
+      age <= 2000 ~ round(runif(1, 976, age - 25)),
+      age <= 3000 ~ round(runif(1, 1976, age - 25)),
+      age <= 4000 ~ round(runif(1, 2976, age - 25)),
+      TRUE        ~ round(runif(1, 3976, age - 25))
     )
   ) %>%
   ungroup()
@@ -66,12 +62,12 @@ toymaker %>%
   count(job_type)
 
 summary(toymaker$work_experience)
-summary(toymaker$skewedAge - toymaker$work_experience)
+summary(toymaker$age - toymaker$work_experience)
 
-min(toymaker$skewedAge - toymaker$work_experience)
-which(toymaker$skewedAge <= toymaker$work_experience)
-all(toymaker$skewedAge > toymaker$work_experience)
-which(is.na(toymaker$skewedAge - toymaker$work_experience))
+min(toymaker$age - toymaker$work_experience)
+which(toymaker$age <= toymaker$work_experience)
+all(toymaker$age > toymaker$work_experience)
+which(is.na(toymaker$age - toymaker$work_experience))
 
 # Professor's help:
 elves_data <- toymaker %>%
