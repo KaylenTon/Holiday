d117397fef173e9c36f89d164bec4835411897d6
Kaylen Ton <kaylent2k@gmail.com>
2025-11-29 20:37:26 -0500

    officialize plot_holiday_countdowns()

--- R/plot_holiday_countdowns.R
+++ R/plot_holiday_countdowns.R
@@ -1,3 +1,10 @@
+#' plot_holiday_countdowns function
+#'
+#' @returns a bar chart in ascending order, visualizing six holidays and how far they are in days
+#' @export
+#'
+#' @examples
+#' plot_holiday_countdowns()
 plot_holiday_countdowns <- function() {
 
   holiday_df <- holiday_df() |>
@@ -6,11 +13,14 @@ plot_holiday_countdowns <- function() {
   holiday_df$holiday <- factor(holiday_df$holiday, levels = holiday_df$holiday)
 
   ggplot2::ggplot(holiday_df) +
-    aes(holiday, countdown) +
+    aes(holiday, countdown, fill = holiday) +
     geom_col() +
     labs(title = "Holidays Coming Up",
          x = "Holiday",
          y = "Days Until") +
+    geom_text(aes(label = countdown),
+              vjust = "bottom",
+              nudge_y = 5) +
     theme_minimal()
 
 }
